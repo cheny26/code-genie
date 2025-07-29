f94673a16f495455b9fb1eeef76387de3ed5c896
cheny26 <a_cheny163@163.com>
2025-07-29 21:00:56 +0800

    注解鉴权

---
+++ src/main/java/com/chen/codegenie/annotation/AuthCheck.java
@@ -0,0 +1,15 @@
+package com.chen.codegenie.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author chen
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuthCheck {
+    String mustRole() default "";
+}
