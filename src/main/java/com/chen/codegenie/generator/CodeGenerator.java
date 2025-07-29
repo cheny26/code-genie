c813fe499ed8c0e1b750f7a732b75490948eb2c7
cheny26 <a_cheny163@163.com>
2025-07-29 21:01:29 +0800

    代码生成器

---
+++ src/main/java/com/chen/codegenie/generator/CodeGenerator.java
@@ -0,0 +1,74 @@
+package com.chen.codegenie.generator;
+
+import cn.hutool.core.lang.Dict;
+import cn.hutool.setting.yaml.YamlUtil;
+import com.mybatisflex.codegen.Generator;
+import com.mybatisflex.codegen.config.GlobalConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.util.Map;
+
+/**
+ * @author chen
+ */
+public class CodeGenerator {
+
+    public static void main(String[] args) {
+        Dict dict = YamlUtil.loadByPath("application.yml");
+        Map<String, Object> dataSourceConfig = dict.getByPath("spring.datasource");
+        String url = String.valueOf(dataSourceConfig.get("url"));
+        String username = String.valueOf(dataSourceConfig.get("username"));
+        String password = String.valueOf(dataSourceConfig.get("password"));
+        // 配置数据源
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        //创建配置内容，两种风格都可以。
+        GlobalConfig globalConfig = createGlobalConfigUseStyle1();
+        //GlobalConfig globalConfig = createGlobalConfigUseStyle2();
+
+        //通过 datasource 和 globalConfig 创建代码生成器
+        Generator generator = new Generator(dataSource, globalConfig);
+
+        //生成代码
+        generator.generate();
+    }
+
+    public static GlobalConfig createGlobalConfigUseStyle1() {
+        //创建配置内容
+        GlobalConfig globalConfig = new GlobalConfig();
+        //设置根包
+        globalConfig.setBasePackage("generate");
+
+        //设置表前缀和只生成哪些表
+        //globalConfig.setTablePrefix("tb_");
+        //globalConfig.setGenerateTable("tb_account", "tb_account_session");
+        globalConfig.setLogicDeleteColumn("is_delete");
+        //设置生成 entity 并启用 Lombok
+        globalConfig.setEntityGenerateEnable(true);
+        globalConfig.setEntityWithLombok(true);
+        //设置项目的JDK版本，项目的JDK为14及以上时建议设置该项，小于14则可以不设置
+        globalConfig.setEntityJdkVersion(21);
+
+
+        // 设置生成 mapper
+        globalConfig.enableMapper();
+        globalConfig.enableMapperXml();
+
+        // 设置生成 service
+        globalConfig.enableService();
+        globalConfig.enableServiceImpl();
+
+        // 设置生成 controller
+        globalConfig.enableController();
+
+        // 设置生成时间和字符串为空，避免多余的代码改动
+        globalConfig.getJavadocConfig()
+                .setAuthor("chen")
+                .setSince("");
+        return globalConfig;
+
+    }
+}
